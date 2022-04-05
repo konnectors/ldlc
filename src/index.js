443f41736d6f0fe96755f546e57d2e5b92f78752
Killian Courvoisier <killian.courvoisier@cozycloud.cc>
2022-04-05 11:55:42 +0200

    feat: adding recaptcha for auth

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ const {
   BaseKonnector,
   requestFactory,
   log,
-  cozyClient
+  cozyClient,
+  solveCaptcha,
+  errors
 } = require('cozy-konnector-libs')
 const jar = require('request').jar()
 const cheerio = require('cheerio')
@@ -39,6 +41,12 @@ async function start(fields) {
   log('info', 'Successfully logged in')
   log('info', 'Fetching the list of bills')
   const ordersPeriods = await parseBills()
+  log(
+    'debug',
+    ordersPeriods
+      ? `Contains ${ordersPeriods.length} periods from parseBills`
+      : `No ordersPeriods founded from parseBills`
+  )
   log('info', 'Parsing list of bills')
   log('info', 'Fetching the list of documents')
   const bills = await getBills(ordersPeriods)
@@ -54,15 +62,8 @@ async function start(fields) {
 
 async function authenticate(username, password) {
   await requestHTML({
-    url: 'https://www.ldlc.com/v4/fr-fr/form/login'
+    url: `https://secure2.ldlc.com/fr-fr/Login/PartialCaptchaByIpOrEmail?email=${username}`
   })
-  await requestHTML({
-    url: 'https://secure2.ldlc.com/fr-fr/Account'
-  })
-  await requestHTML({
-    url: 'https://secure2.ldlc.com/fr-fr/Login/Login?returnUrl=/fr-fr/Account'
-  })
-
   const $ = await requestHTML({
     url: 'https://secure2.ldlc.com/fr-fr/Login/Login?returnUrl=/fr-fr/Account',
     headers: { 'x-requested-with': 'XMLHttpRequest' }
@@ -70,22 +71,39 @@ async function authenticate(username, password) {
   const reqVerifToken = $('input[name="__RequestVerificationToken"]').attr(
     'value'
   )
-
-  try {
-    const resp = await requestJSON({
-      url: 'https://secure2.ldlc.com/fr-fr/Login/Login?returnUrl=/fr-fr/Account',
-      method: 'POST',
-      formSelector: '#loginForm',
-      form: {
-        __RequestVerificationToken: `${reqVerifToken}`,
-        Email: username,
-        Password: password,
-        LongAuthenticationDuration: false
-      }
-    })
-    return resp
-  } catch (err) {
-    log('err', err)
+  if (!reqVerifToken) {
+    throw new Error(
+      'There is no verification token, cannot connect the account'
+    )
+  }
+  const gRecaptcha = await solveCaptcha({
+    websiteKey: '6LdgbxgUAAAAAHvdiswvziUz_XecIWb1sWTW7pnz',
+    websiteURL: 'https://www.ldlc.com/'
+  })
+  const resp = await requestJSON({
+    url: 'https://secure2.ldlc.com/fr-fr/Login/Login?returnUrl=/fr-fr/Account',
+    method: 'POST',
+    formSelector: '#loginForm',
+    form: {
+      __RequestVerificationToken: `${reqVerifToken}`,
+      Email: username,
+      Password: password,
+      LongAuthenticationDuration: false,
+      'g-recaptcha-response': gRecaptcha
+    }
+  })
+  if (resp.match(/href="\/fr-fr\/Account\/Logout/g)) {
+    log('debug', 'Login successful, sending OK to cozy-libs')
+    return reqVerifToken
+  }
+  if (resp.match(/<title>Connexion<\/title>/g)) {
+    if (resp.match(/Identifiants incorrects/g)) {
+      log('debug', 'Something went wrong with your credentials')
+      throw new Error(errors.LOGIN_FAILED)
+    } else {
+      log('debug', 'Something went wrong with the website')
+      throw new Error(errors.VENDOR_DOWN)
+    }
   }
 }
 
@@ -94,6 +112,15 @@ async function parseBills() {
     url: 'https://secure2.ldlc.com/fr-fr/Orders/CompletedOrdersPeriodSelection',
     method: 'POST'
   })
+  log('debug', 'First getOrders')
+  log(
+    'debug',
+    getOrders
+      ? `getOrders is ${getOrders.length} long and of type ${typeof getOrders}`
+      : `No getOrders`
+  )
+
+  log('debug', 'Returning order list')
   return getOrders
 }
 
